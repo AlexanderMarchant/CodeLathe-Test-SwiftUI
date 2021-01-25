f4466fbf0c3a941209085638fb87d47f0a77d466
AlexanderMarchant <alexmarchant@Alexs-MacBook-Pro.local>
2021-01-25 19:44:18 +0000

    Add Appcolours

---
+++ Codelathe-Test-SwiftUI/Extensions/Color/AppColours.swift
@@ -0,0 +1,41 @@
+//
+//  AppColours.swift
+//  Codelathe-Test-SwiftUI
+//
+//  Created by Alex Marchant on 25/01/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static var background: Color  {
+        return Color("background")
+    }
+    
+    static var body: Color  {
+        return Color("body")
+    }
+    
+    static var borders: Color  {
+        return Color("borders")
+    }
+    
+    static var codeLathe: Color  {
+        return Color("codeLathe")
+    }
+    
+    static var galleryCellBorder: Color  {
+        return Color("galleryCellBorder")
+    }
+    
+    static var navigationBarButtonFill: Color  {
+        return Color("navigationBarButtonFill")
+    }
+    
+    static var skillCellBackground: Color  {
+        return Color("skillCellBackground")
+    }
+    
+}
