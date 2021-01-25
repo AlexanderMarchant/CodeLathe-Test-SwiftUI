8f29cfb0456babe4ec53946bc9c9a258c8414f10
AlexanderMarchant <alexmarchant@Alexs-MacBook-Pro.local>
2021-01-25 19:35:07 +0000

    Initial commit

---
+++ Codelathe-Test-SwiftUI/Codelathe_Test_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codelathe_Test_SwiftUIApp.swift
+//  Codelathe-Test-SwiftUI
+//
+//  Created by Alex Marchant on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Codelathe_Test_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
